27a6bf20d542d36aa3f111d784808f63f23cefbf
Lucas Ledsham <lucasnunesleal@gmail.com>
2024-09-15 16:36:36 -0300

    NumerosUmACem

---
+++ exerciciosFor/11_numerosUmACem/numerosUmACem/src/numerosUmACem.java
@@ -0,0 +1,10 @@
+// Escreva um programa que imprima todos os números de 1 a 100 utilizando o loop for.
+
+public class numerosUmACem {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
