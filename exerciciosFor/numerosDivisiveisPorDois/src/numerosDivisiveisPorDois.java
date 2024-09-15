7ff919aab40ecc3c821b2e3862851c79329d41d6
Lucas Ledsham <lucasnunesleal@gmail.com>
2024-09-15 02:09:35 -0300

    exercício divisível por dois

--- exerciciosFor/numerosDivisiveisPorDois/src/numerosDivisiveisPorDois.java
+++ exerciciosFor/numerosDivisiveisPorDois/src/numerosDivisiveisPorDois.java
@@ -1,5 +1,17 @@
+// Itere por essa lista e imprima todos os números que são divisíveis por 2.
+
 public class numerosDivisiveisPorDois {
     public static void main(String[] args) {
-        
+        int[] numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int i = 0; i < numeros.length; i++) {
+            int numero = numeros[i];
+            int modulo = numero % 2;
+            
+            if (modulo == 0) {
+                System.err.println(numero + " é divisível por dois.");
+            }
+
+        }
     }
 }
