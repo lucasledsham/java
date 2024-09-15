1e8d5a1d7ad4ab0081d075120bb6a4265888f989
Lucas Ledsham <lucasnunesleal@gmail.com>
2024-09-15 02:55:59 -0300

    Números terminando em 5

---
+++ exerciciosFor/05_numerosTerminandoEmCinco/numerosTerminandoEmCinco/src/App.java
@@ -0,0 +1,17 @@
+// Itere por essa lista e imprima todos os números que terminam em 5.
+
+public class App {
+    public static void main(String[] args) {
+        int[] lista = {5, 10, 15, 20, 25, 30, 35, 40, 45, 50};
+        
+        System.out.println("Os números que terminam em 5 são: ");
+
+        for (int i = 0; i < lista.length; i++) {
+            int modulo = lista[i] % 10;
+
+            if (modulo == 5) {
+                System.out.println(lista[i]);
+            }
+        }
+    }
+}
