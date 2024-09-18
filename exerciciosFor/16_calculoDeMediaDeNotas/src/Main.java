36eccd49f620b197a597e98056e1b740bbdfdef4
Lucas Ledsham <lucasnunesleal@gmail.com>
2024-09-18 13:33:47 -0300

    .

---
+++ exerciciosFor/16_calculoDeMediaDeNotas/src/Main.java
@@ -0,0 +1,11 @@
+// Programa em Java que:
+//Solicite ao usuário o número de notas que ele deseja inserir.
+//Use um loop for para permitir que o usuário insira essas notas.
+//Calcule a média das notas inseridas.
+//Exiba a média ao final.
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
