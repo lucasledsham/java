4a15d04f1cbee349831086a3de40a18f584f70d5
Lucas Ledsham <lucasnunesleal@gmail.com>
2024-10-10 21:24:52 -0300

    1.1.15

---
+++ 300-ideias-exercicio/1.1.15/src/Exercicio1115.java
@@ -0,0 +1,17 @@
+//1.1.15 Menu - Elabore um programa que mostre o seguinte menu na tela:
+//
+// Cadastro de Clientes
+//
+// 0 - Fim
+// 1 - Inclui
+// 2 - Altera
+// 3 - Exclui
+// 4 - Consulta
+//
+// Opção: _
+
+public class Exercicio1115 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
